0830cd6f7dd33b387e6d3318f9cde3672e10cae9
jelinn <jelinn@gmail.com>
2020-08-26 10:25:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"
+    organization = "JLOrg"
 
     workspaces {
-      name = "learn-terraform-pipelines-consul"
+      name = "k8s-tf-demo-consul"
     }
   }
 }
